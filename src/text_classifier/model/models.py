f414493d94c5a611ef9326cafe5837ff13673a80
john <96323078+JohnStephens1@users.noreply.github.com>
2026-07-26 06:26:45 +0000

    chore: add model class without instance

--- src/text_classifier/model/models.py
+++ src/text_classifier/model/models.py
@@ -1,58 +1,118 @@
 from abc import ABC
 from collections.abc import Mapping
+from types import MappingProxyType
 from typing import Any
 
 from scipy.stats import loguniform, randint, uniform  # type: ignore
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 
+# class ModelBase(ABC):
+#     def __init__(
+#         self,
+#         default_params: dict[str, Any],
+#         default_param_dist: dict[str, Any],
+#         ModelType: Any,
+#         instantiate_w_default_params: bool = False,
+#     ) -> None:
+#         super().__init__()
+#         self.default_params = default_params
+#         self.default_param_dist = default_param_dist
+#         self.default_params_w_model_prefix = prefix_dict_keys_with_model(default_params)
+#         self.default_param_dist_w_model_prefix = prefix_dict_keys_with_model(
+#             default_param_dist
+#         )
+#         self.model = (
+#             ModelType(**self.default_params)
+#             if instantiate_w_default_params
+#             else ModelType()
+#         )
+
+
+# def prefix_dict_keys_with_model(dic: dict[str, Any]) -> dict[str, Any]:
+#     return {f"model__{k}": v for k, v in dic.items()}
+
+
+# class RandomForestModel(ModelBase):
+#     def __init__(self, instantiate_w_default_params: bool = False) -> None:
+#         super().__init__(
+#             {
+#                 "n_estimators": 10,
+#                 "max_depth": 5,
+#                 "min_samples_split": 3,
+#                 "min_samples_leaf": 2,
+#                 "max_features": "sqrt",
+#             },
+#             {
+#                 "n_estimators": [10],
+#                 "max_depth": [5, 10],
+#                 "min_samples_split": [3, 8],
+#                 "min_samples_leaf": [2, 4],
+#                 "max_features": ["sqrt"],
+#             },
+#             RandomForestClassifier,
+#             instantiate_w_default_params,
+#         )
+
+
+def prefix_mpt_keys_with_model(
+    dic: MappingProxyType[str, Any],
+) -> MappingProxyType[str, Any]:
+    return MappingProxyType({f"model__{k}": v for k, v in dic.items()})
+
+
+def get_model_params_dist(
+    params: MappingProxyType[str, Any], param_dist: MappingProxyType[str, Any]
+) -> tuple[
+    MappingProxyType[str, Any],
+    MappingProxyType[str, Any],
+    MappingProxyType[str, Any],
+    MappingProxyType[str, Any],
+]:
+    params_w_model_prefix = prefix_mpt_keys_with_model(params)
+    param_dist_w_model_prefix = prefix_mpt_keys_with_model(param_dist)
+
+    return params, params_w_model_prefix, param_dist, param_dist_w_model_prefix
+
 
 class ModelBase(ABC):
-    def __init__(
-        self,
-        default_params: dict[str, Any],
-        default_param_dist: dict[str, Any],
-        ModelType: Any,
-        instantiate_w_default_params: bool = False,
-    ) -> None:
-        super().__init__()
-        self.default_params = default_params
-        self.default_param_dist = default_param_dist
-        self.default_params_w_model_prefix = prefix_dict_keys_with_model(default_params)
-        self.default_param_dist_w_model_prefix = prefix_dict_keys_with_model(
-            default_param_dist
-        )
-        self.model = (
-            ModelType(**self.default_params)
-            if instantiate_w_default_params
-            else ModelType()
-        )
+    _model: Any
+    params_w_model_prefix: MappingProxyType[str, Any]
 
-
-def prefix_dict_keys_with_model(dic: dict[str, Any]) -> dict[str, Any]:
-    return {f"model__{k}": v for k, v in dic.items()}
+    @classmethod
+    def get_model(cls, with_params: bool):
+        if with_params:
+            return cls._model(**cls.params_w_model_prefix)
+        return cls._model()
 
 
 class RandomForestModel(ModelBase):
-    def __init__(self, instantiate_w_default_params: bool = False) -> None:
-        super().__init__(
-            {
-                "n_estimators": 10,
-                "max_depth": 5,
-                "min_samples_split": 3,
-                "min_samples_leaf": 2,
-                "max_features": "sqrt",
-            },
-            {
-                "n_estimators": [10],
-                "max_depth": [5, 10],
-                "min_samples_split": [3, 8],
-                "min_samples_leaf": [2, 4],
-                "max_features": ["sqrt"],
-            },
-            RandomForestClassifier,
-            instantiate_w_default_params,
+    _model = RandomForestClassifier
+    params, params_w_model_prefix, param_dist, param_dist_w_model_prefix = (
+        get_model_params_dist(
+            MappingProxyType(
+                {
+                    "n_estimators": 10,
+                    "max_depth": 5,
+                    "min_samples_split": 3,
+                    "min_samples_leaf": 2,
+                    "max_features": "sqrt",
+                }
+            ),
+            MappingProxyType(
+                {
+                    "n_estimators": [10],
+                    "max_depth": [5, 10],
+                    "min_samples_split": [3, 8],
+                    "min_samples_leaf": [2, 4],
+                    "max_features": ["sqrt"],
+                }
+            ),
         )
+    )
+
+
+# RandomForestModel.get_model(False)
 
 
 def get_xgboost_param_distribution() -> dict[str, Any]:
