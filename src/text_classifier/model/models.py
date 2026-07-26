b103649a057d30f09887490acae8dac901ae791d
john <96323078+JohnStephens1@users.noreply.github.com>
2026-07-26 03:59:49 +0000

    WIP: adding Model class

--- src/text_classifier/model/models.py
+++ src/text_classifier/model/models.py
@@ -1,9 +1,68 @@
+from abc import ABC, abstractmethod
 from collections.abc import Mapping
 from typing import Any
 
 from scipy.stats import loguniform, randint, uniform  # type: ignore
+from sklearn.base import BaseEstimator
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
+
+
+class Model(ABC):
+    @abstractmethod
+    def get_default_params(self) -> dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def get_param_distribution(self) -> dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def get_model(self) -> BaseEstimator:
+        pass
+
+
+def prefix_dict_keys_with_model(dic: dict[str, Any]) -> dict[str, Any]:
+    return {f"model__{k}": v for k, v in dic.items()}
+
+
+class RandomForestModel(Model):
+    def __init__(self) -> None:
+        super().__init__()
+        self.default_params = {
+            "n_estimators": 10,
+            "max_depth": 5,
+            "min_samples_split": 3,
+            "min_samples_leaf": 2,
+            "max_features": "sqrt",
+        }
+        self.default_param_dist = {
+            "n_estimators": [10],
+            "max_depth": [5, 10],
+            "min_samples_split": [3, 8],
+            "min_samples_leaf": [2, 4],
+            "max_features": ["sqrt"],
+        }
+        self.model: RandomForestClassifier
+
+    def get_default_params(self, add_prefix_for_pipe: bool = True) -> dict[str, Any]:
+        return (
+            prefix_dict_keys_with_model(self.default_params)
+            if add_prefix_for_pipe
+            else self.default_params
+        )
+
+    def get_param_distribution(
+        self, add_prefix_for_pipe: bool = True
+    ) -> dict[str, Any]:
+        return (
+            prefix_dict_keys_with_model(self.default_param_dist)
+            if add_prefix_for_pipe
+            else self.default_param_dist
+        )
+
+    def get_model(self):
+        return RandomForestClassifier()
 
 
 def get_xgboost_param_distribution() -> dict[str, Any]:
