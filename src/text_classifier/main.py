7c6254b9cbc8fd0491c3ba60474bdcb9b51b5504
john <96323078+JohnStephens1@users.noreply.github.com>
2026-07-27 05:53:03 +0000

    fix: implement train_data in main

--- src/text_classifier/main.py
+++ src/text_classifier/main.py
@@ -1,12 +1,12 @@
-from text_classifier.data.model import get_model_data
+from text_classifier.data.model import get_encoder_train_data
 
 
 def main():
-    _, _, X_train, X_test, y_train, y_test = get_model_data()
-    print(f"X_train: \n{X_train.head()}\n")
-    print(f"X_test: \n{X_test.head()}\n")
-    print(f"y_train: \n{y_train}\n")
-    print(f"y_test: \n{y_test}\n")
+    _, train_data = get_encoder_train_data()
+    print(f"X_train: \n{train_data.X_train.head()}\n")
+    print(f"X_test: \n{train_data.X_test.head()}\n")
+    print(f"y_train: \n{train_data.y_train}\n")
+    print(f"y_test: \n{train_data.y_test}\n")
 
 
 if __name__ == "__main__":
