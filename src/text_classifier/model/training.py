4d79ee29b591a985813bae8ba851bee9f5041755
john <96323078+JohnStephens1@users.noreply.github.com>
2026-07-27 17:04:31 +0000

    chore: fully implement ModelBase into train

--- src/text_classifier/model/training.py
+++ src/text_classifier/model/training.py
@@ -1,12 +1,10 @@
 from typing import Any
 
 import mlflow
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.preprocessing import LabelEncoder
 
 from text_classifier.data.model import get_encoder_train_data
 from text_classifier.evaluation.metrics import get_classification_metrics
-from text_classifier.model.model_selection import get_search_full
 from text_classifier.model.models import (
     ModelBase,
 )
@@ -35,53 +33,40 @@ from text_classifier.schema import TrainingData
 def train_core(
     train_data: TrainingData,
     my_model: ModelBase,
-) -> tuple[TrainingData, RandomizedSearchCV | GridSearchCV, dict[str, Any]]:
-    search = get_search_full(
-        my_model.model_cls,
-        my_model.search_param_dist_w_model_prefix,
-        my_model.search_cls,
-        my_model.search_params,
-        my_model.model_default_params,
-    )
-    search.fit(train_data.X_train, train_data.y_train)
+) -> tuple[TrainingData, ModelBase, dict[str, Any]]:
+    my_model.search.fit(train_data.X_train, train_data.y_train)
 
     metrics = get_classification_metrics(
         train_data.y_test,
-        search.predict(train_data.X_test),
-        search.predict_proba(train_data.X_test),
+        my_model.search.predict(train_data.X_test),
+        my_model.search.predict_proba(train_data.X_test),
         prefix="test",
     )
 
-    return train_data, search, metrics
+    return train_data, my_model, metrics
 
 
 def train_w_tracking(
     train_data: TrainingData,
     my_model: ModelBase,
-) -> tuple[TrainingData, RandomizedSearchCV | GridSearchCV, dict[str, Any]]:
+) -> tuple[TrainingData, ModelBase, dict[str, Any]]:
     mlflow.set_tracking_uri("http://localhost:5000")
     mlflow.set_experiment("domain-classification")
     mlflow.sklearn.autolog()  # type: ignore
 
     with mlflow.start_run():
-        train_data, search, metrics = train_core(train_data, my_model)
+        train_data, my_model, metrics = train_core(train_data, my_model)
 
         mlflow.log_metrics(metrics)
-        # mlflow.log_metric("cv_score", search.best_score_)
-        # mlflow.sklearn.log_model(search.best_estimator_, "model")
-        # mlflow.sklearn.log_model(search, "search")
-
         mlflow.set_tags({"model": my_model.model_name, "search": my_model.search_name})
 
-    return train_data, search, metrics
+    return train_data, my_model, metrics
 
 
 def train_qm(
     my_model: ModelBase,
-) -> tuple[
-    LabelEncoder, TrainingData, RandomizedSearchCV | GridSearchCV, dict[str, Any]
-]:
+) -> tuple[LabelEncoder, TrainingData, ModelBase, dict[str, Any]]:
     encoder, train_data = get_encoder_train_data()
-    train_data, search, metrics = train_w_tracking(train_data, my_model)
+    train_data, my_model, metrics = train_w_tracking(train_data, my_model)
 
-    return encoder, train_data, search, metrics
+    return encoder, train_data, my_model, metrics
