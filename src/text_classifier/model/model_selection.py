3ab2645bb78eecd2cce4b2261159e77cb1d2abe8
john <96323078+JohnStephens1@users.noreply.github.com>
2026-07-27 17:10:50 +0000

    refactor: comment out unused code

--- src/text_classifier/model/model_selection.py
+++ src/text_classifier/model/model_selection.py
@@ -4,8 +4,6 @@ from sklearn.base import BaseEstimator
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, StratifiedKFold
 from sklearn.pipeline import Pipeline
 
-from text_classifier.model.pipeline import get_model_pipe
-
 
 def get_model(
     model_cls: type[BaseEstimator], model_params: dict[str, Any]
@@ -15,34 +13,6 @@ def get_model(
 
 def get_cv_splitter() -> StratifiedKFold:
     return StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
-
-
-def get_random_search(
-    pipe: Pipeline,
-    param_distribution: dict[str, Any],
-) -> RandomizedSearchCV:
-    return RandomizedSearchCV(
-        pipe,
-        param_distribution,
-        n_iter=10,
-        cv=get_cv_splitter(),
-        scoring="f1_macro",
-        random_state=42,
-        verbose=1,
-    )
-
-
-def get_grid_search(
-    pipe: Pipeline,
-    param_distribution: dict[str, Any],
-) -> GridSearchCV:
-    return GridSearchCV(
-        pipe,
-        param_distribution,
-        cv=get_cv_splitter(),
-        scoring="f1_macro",
-        verbose=1,
-    )
 
 
 def get_search(
@@ -62,23 +32,51 @@ def get_search(
     )
 
 
-def get_search_full(
-    model_cls: type[BaseEstimator],
-    param_dist: dict[str, Any],
-    Search: type[RandomizedSearchCV | GridSearchCV],
-    search_params: dict[str, Any],
-    model_params: dict[str, Any] | None = None,
-) -> RandomizedSearchCV | GridSearchCV:
-    if model_params is None:
-        model_params = {}
+# def get_random_search(
+#     pipe: Pipeline,
+#     param_distribution: dict[str, Any],
+# ) -> RandomizedSearchCV:
+#     return RandomizedSearchCV(
+#         pipe,
+#         param_distribution,
+#         n_iter=10,
+#         cv=get_cv_splitter(),
+#         scoring="f1_macro",
+#         random_state=42,
+#         verbose=1,
+#     )
 
-    model = get_model(model_cls, model_params)
-    pipe = get_model_pipe(model)
-    search = get_search(
-        pipe,
-        Search,
-        param_dist,
-        search_params,
-    )
 
-    return search
+# def get_grid_search(
+#     pipe: Pipeline,
+#     param_distribution: dict[str, Any],
+# ) -> GridSearchCV:
+#     return GridSearchCV(
+#         pipe,
+#         param_distribution,
+#         cv=get_cv_splitter(),
+#         scoring="f1_macro",
+#         verbose=1,
+#     )
+
+
+# def get_search_full(
+#     model_cls: type[BaseEstimator],
+#     param_dist: dict[str, Any],
+#     Search: type[RandomizedSearchCV | GridSearchCV],
+#     search_params: dict[str, Any],
+#     model_params: dict[str, Any] | None = None,
+# ) -> RandomizedSearchCV | GridSearchCV:
+#     if model_params is None:
+#         model_params = {}
+
+#     model = get_model(model_cls, model_params)
+#     pipe = get_model_pipe(model)
+#     search = get_search(
+#         pipe,
+#         Search,
+#         param_dist,
+#         search_params,
+#     )
+
+#     return search
