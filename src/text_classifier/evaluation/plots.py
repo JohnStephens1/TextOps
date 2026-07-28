be0a6a7459f33b649bf7c247342302fc54328d8f
john <96323078+JohnStephens1@users.noreply.github.com>
2026-07-28 06:13:51 +0000

    chore: add get_roc_curve_fig fn

--- src/text_classifier/evaluation/plots.py
+++ src/text_classifier/evaluation/plots.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
-from numpy.typing import ArrayLike
-from sklearn.metrics import ConfusionMatrixDisplay
+import numpy as np
+from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay
+from sklearn.preprocessing import label_binarize
 
 
-def get_confusion_matrix_fig(y_true: ArrayLike, y_pred: ArrayLike):
+def get_confusion_matrix_fig(y_true: np.typing.ArrayLike, y_pred: np.typing.ArrayLike):
     fig, ax = plt.subplots(figsize=(5, 5))
 
     ConfusionMatrixDisplay.from_predictions(
@@ -17,6 +18,35 @@ def get_confusion_matrix_fig(y_true: ArrayLike, y_pred: ArrayLike):
         "Confusion Matrix",
         pad=16,
     )
+
+    plt.tight_layout()
+
+    return fig
+
+
+# TODO integrate encoder
+def get_roc_curve_fig(
+    y_true: np.typing.NDArray[np.float64], y_proba: np.typing.NDArray[np.float64]
+):
+    fig, ax = plt.subplots(figsize=(5, 5))
+
+    n_classes = 4
+    y_test_bin = np.array(
+        label_binarize(y_true, classes=range(n_classes)), dtype=np.float64
+    )
+
+    for i in range(n_classes):
+        RocCurveDisplay.from_predictions(
+            y_test_bin[:, i],
+            y_proba[:, i],
+            # set proper class labels using encoder
+            name=f"Class {i}",
+            ax=ax,
+        )
+
+    ax.set_title("ROC Curve", pad=16)
+    ax.set_xlabel("False Positive Rate")
+    ax.set_ylabel("True Positive Rate")
 
     plt.tight_layout()
 
