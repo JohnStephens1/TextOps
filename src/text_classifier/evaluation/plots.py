f161b1d106e7bdd696fa2a0bbe9293accc86b49c
john <96323078+JohnStephens1@users.noreply.github.com>
2026-07-29 10:08:46 +0000

    chore: update get_model_eval_figs to return a dict instead of a list

--- src/text_classifier/evaluation/plots.py
+++ src/text_classifier/evaluation/plots.py
@@ -181,10 +181,12 @@ def get_model_eval_figs(
     y_pred: np.typing.NDArray[np.float64],
     y_proba: np.typing.NDArray[np.float64],
     encoder: LabelEncoder,
-) -> tuple[Figure, Figure, Figure]:
-    return (
-        get_confusion_matrix_fig(y_true, y_pred),
-        get_roc_curve_fig(y_true, y_proba, encoder),
-        get_precision_recall_curve_fig(y_true, y_proba, encoder),
+) -> dict[str, Figure]:
+    return {
+        "confusion_matrix": get_confusion_matrix_fig(y_true, y_pred),
+        "roc_curve": get_roc_curve_fig(y_true, y_proba, encoder),
+        "precision_recall_curve": get_precision_recall_curve_fig(
+            y_true, y_proba, encoder
+        ),
         # could add feature importance fig
-    )
+    }
