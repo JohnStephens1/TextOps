fabc53a140cfaf4cd7b29a0cf4cd43a19e37794b
john <96323078+JohnStephens1@users.noreply.github.com>
2026-07-29 06:21:18 +0000

    chore: add get_precision_recall_curve_fig fn

--- src/text_classifier/evaluation/plots.py
+++ src/text_classifier/evaluation/plots.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.figure import Figure
-from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay
+from sklearn.metrics import (
+    ConfusionMatrixDisplay,
+    PrecisionRecallDisplay,
+    RocCurveDisplay,
+)
 from sklearn.preprocessing import LabelEncoder, label_binarize
 
 
@@ -47,6 +51,35 @@ def get_roc_curve_fig(
     ax.set_title("ROC Curve", pad=16)
     ax.set_xlabel("False Positive Rate")
     ax.set_ylabel("True Positive Rate")
+
+    plt.tight_layout()
+
+    return fig
+
+
+def get_precision_recall_curve_fig(
+    y_true: np.typing.NDArray[np.float64],
+    y_proba: np.typing.NDArray[np.float64],
+    encoder: LabelEncoder,
+) -> Figure:
+    fig, ax = plt.subplots(figsize=(5, 5))
+
+    n_classes = encoder.classes_.shape[0]
+    y_test_bin = np.asarray(
+        label_binarize(y_true, classes=range(n_classes)), dtype=np.float64
+    )
+
+    for i in range(n_classes):
+        PrecisionRecallDisplay.from_predictions(
+            y_test_bin[:, i],
+            y_proba[:, i],
+            name=f"{encoder.classes_[i]}",
+            ax=ax,
+        )
+
+    ax.set_title("Precision-Recall Curve", pad=16)
+    ax.set_xlabel("Recall")
+    ax.set_ylabel("Precision")
 
     plt.tight_layout()
 
