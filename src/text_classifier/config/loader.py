18a820e31231f4302c0e5aeffeeee1907e9763e8
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-03 10:13:06 +0000

    docs: add docstrings to loader.py fns

--- src/text_classifier/config/loader.py
+++ src/text_classifier/config/loader.py
@@ -8,13 +8,30 @@ from sklearn.model_selection import GridSearchCV
 from text_classifier.config.config import PROJECT_ROOT
 
 
-def load_raw_config(config_path: Path = PROJECT_ROOT / "params.yaml"):
+def load_raw_config(config_path: Path = PROJECT_ROOT / "params.yaml") -> dict[str, Any]:
+    """gets a dictionary loaded from the specified config_path, params.yaml by default
+
+    Args:
+        config_path (Path, optional): path to the yaml file. Defaults to PROJECT_ROOT/"params.yaml".
+
+    Returns:
+        dict[str, Any]: the dict loaded from config_path
+    """
 
     with open(config_path, "r") as f:
         return yaml.safe_load(f)
 
 
 def load_and_parse_config() -> dict[str, Any]:
+    """loads data from params.yaml and returns a parsed version, directly usable my ModelBase
+
+    Raises:
+        KeyError: raises when "active_model" doesn't match a defined model category
+
+    Returns:
+        dict[str, Any]: the parsed dict, directly usable by ModelBase
+    """
+
     le_config = load_raw_config()
     target_model_dict = le_config["models"][le_config["active_model"]]
 
@@ -27,6 +44,15 @@ def load_and_parse_config() -> dict[str, Any]:
 
 
 def parse_random_forest_config(model_dict: dict[str, Any]) -> dict[str, Any]:
+    """parses a model dict loaded from params.yaml to be directly usable by ModelBase
+
+    Args:
+        model_dict (dict[str, Any]): the model related part of thedict loaded from params.yaml, e.g. params.yaml["models"]["random_forest"]
+
+    Returns:
+        dict[str, Any]: the parsed dict, directly usable by ModelBase
+    """
+
     result_dict = {}
 
     result_dict["model_default_params"] = model_dict["default_params"]
