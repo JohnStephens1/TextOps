7f8353223f2aedb5ab7d742b0a5fb3915542bf51
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-03 13:36:30 +0000

    chore: add FEATURE_DATASET_PATH

--- src/text_classifier/config/config.py
+++ src/text_classifier/config/config.py
@@ -5,6 +5,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[3]
 DATASETS_DIR = PROJECT_ROOT / "data/datasets"
 RAW_DATASET_PATH = DATASETS_DIR / "raw_dataset.csv"
 PREPROCESSED_DATASET_PATH = DATASETS_DIR / "preprocessed_dataset.parquet"
+FEATURE_DATASET_PATH = DATASETS_DIR / "feature_dataset.parquet"
 
 EMBEDDINGS_PATH = PROJECT_ROOT / "data/embeddings/text_embeddings.npz"
 
