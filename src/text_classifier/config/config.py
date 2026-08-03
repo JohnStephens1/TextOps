41419fff2ab691e62f9812e6d99172714edf3dc3
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-03 12:57:57 +0000

    chore: add PREPROCESSED_DATASET_PATH to config

--- src/text_classifier/config/config.py
+++ src/text_classifier/config/config.py
@@ -4,6 +4,8 @@ PROJECT_ROOT = Path(__file__).resolve().parents[3]
 
 DATASETS_DIR = PROJECT_ROOT / "data/datasets"
 RAW_DATASET_PATH = DATASETS_DIR / "raw_dataset.csv"
+PREPROCESSED_DATASET_PATH = DATASETS_DIR / "preprocessed_dataset.parquet"
+
 EMBEDDINGS_PATH = PROJECT_ROOT / "data/embeddings/text_embeddings.npz"
 
 EMBEDDING_MODEL_STR = "all-MiniLM-L6-v2"
