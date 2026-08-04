4abf15a6c858a8228c1670ab495c4e32eaf3bdd7
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-04 15:59:22 +0000

    chore: remove run_metadata logging

--- src/text_classifier/evaluation/eval.py
+++ src/text_classifier/evaluation/eval.py
@@ -1,13 +1,11 @@
-import json
 import tempfile
 from pathlib import Path
 
-import joblib
+import joblib  # type: ignore
 import mlflow
 from matplotlib.figure import Figure
 from sklearn.preprocessing import LabelEncoder
 
-from text_classifier.config.config import TRAIN_METADATA_PATH
 from text_classifier.evaluation.metrics import get_classification_metrics
 from text_classifier.evaluation.plots import get_model_eval_figs
 from text_classifier.model.predictions import get_predictions_w_encoder
@@ -25,19 +23,6 @@ def log_encoder(
 
         joblib.dump(encoder, encoder_path)
         mlflow.log_artifact(str(encoder_path), artifact_path=artifact_dir_name)
-
-
-# do i want this
-def log_train_metadata(run: mlflow.ActiveRun) -> None:
-    metadata = {
-        "run_id": run.info.run_id,
-        "experiment_id": run.info.experiment_id,
-        "artifact_uri": run.info.artifact_uri,
-        "model_uri": f"runs:/{run.info.run_id}/model",
-    }
-
-    with open(TRAIN_METADATA_PATH, "w") as f:
-        json.dump(metadata, f, indent=2)
 
 
 def log_metrics_figs(metrics: dict[str, float], figs: dict[str, Figure]) -> None:
@@ -71,11 +56,10 @@ def evaluate(
 
     print("trying 2 connect")  # TODO replace with logger
 
-    with mlflow.start_run(run_id) as run:
+    with mlflow.start_run(run_id):
         metrics, figs = get_metrics_figs(model, train_data, encoder)
 
         log_metrics_figs(metrics, figs)
         log_encoder(encoder)
-        log_train_metadata(run)
 
     return train_data, model, metrics, figs
