7feccf5724e51370dd60f387892414231d551b87
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-04 15:33:10 +0000

    chore: remove remaining metric related fns from train and comment out unused fns

--- src/text_classifier/model/training.py
+++ src/text_classifier/model/training.py
@@ -1,12 +1,8 @@
-import json
 import logging
-import tempfile
-from pathlib import Path
 
 import joblib  # type: ignore
 import mlflow
 import pandas as pd
-from matplotlib.figure import Figure
 from sklearn.base import BaseEstimator
 from sklearn.preprocessing import LabelEncoder
 
@@ -14,16 +10,12 @@ from text_classifier.config.config import (
     ENCODER_ARTIFACT_PATH,
     MODEL_ARTIFACT_PATH,
     RUN_ID_ARTIFACT_PATH,
-    TRAIN_METADATA_PATH,
 )
 from text_classifier.data.train_data import get_encoder_train_data
-from text_classifier.evaluation.metrics import get_classification_metrics
-from text_classifier.evaluation.plots import get_model_eval_figs
 from text_classifier.model.models import (
     ModelBase,
     get_model_from_config,
 )
-from text_classifier.model.predictions import get_predictions_w_encoder
 from text_classifier.schema import TrainingData
 
 logger = logging.getLogger(__name__)
@@ -36,52 +28,28 @@ def save_model_encoder_run_id(model: BaseEstimator, encoder: LabelEncoder, run_i
     RUN_ID_ARTIFACT_PATH.write_text(run_id, encoding="utf-8")
 
 
-def log_encoder(
-    encoder: LabelEncoder,
-    encoder_file_name: str = "label_encoder.joblib",
-    artifact_dir_name: str = "preprocessing",
-) -> None:
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        encoder_path = Path(tmp_dir) / encoder_file_name
+# def log_encoder(
+#     encoder: LabelEncoder,
+#     encoder_file_name: str = "label_encoder.joblib",
+#     artifact_dir_name: str = "preprocessing",
+# ) -> None:
+#     with tempfile.TemporaryDirectory() as tmp_dir:
+#         encoder_path = Path(tmp_dir) / encoder_file_name
 
-        joblib.dump(encoder, encoder_path)
-        mlflow.log_artifact(str(encoder_path), artifact_path=artifact_dir_name)
-
-
-def log_metrics_figs(metrics: dict[str, float], figs: dict[str, Figure]) -> None:
-    mlflow.log_metrics(metrics)
-
-    for fig_name, fig in figs.items():
-        mlflow.log_figure(fig, f"figures/{fig_name}.png")
+#         joblib.dump(encoder, encoder_path)
+#         mlflow.log_artifact(str(encoder_path), artifact_path=artifact_dir_name)
 
 
-def log_train_metadata(run: mlflow.ActiveRun) -> None:
-    metadata = {
-        "run_id": run.info.run_id,
-        "experiment_id": run.info.experiment_id,
-        "artifact_uri": run.info.artifact_uri,
-        "model_uri": f"runs:/{run.info.run_id}/model",
-    }
+# def log_train_metadata(run: mlflow.ActiveRun) -> None:
+#     metadata = {
+#         "run_id": run.info.run_id,
+#         "experiment_id": run.info.experiment_id,
+#         "artifact_uri": run.info.artifact_uri,
+#         "model_uri": f"runs:/{run.info.run_id}/model",
+#     }
 
-    with open(TRAIN_METADATA_PATH, "w") as f:
-        json.dump(metadata, f, indent=2)
-
-
-def get_metrics_figs(
-    my_model: ModelBase, train_data: TrainingData, encoder: LabelEncoder
-) -> tuple[dict[str, float], dict[str, Figure]]:
-    predictions_w_encoder = get_predictions_w_encoder(
-        my_model.search, train_data.X_test, train_data.y_test, encoder
-    )
-
-    metrics = get_classification_metrics(
-        predictions_w_encoder.predictions,
-        prefix="test",
-    )
-
-    figs = get_model_eval_figs(predictions_w_encoder)
-
-    return metrics, figs
+#     with open(TRAIN_METADATA_PATH, "w") as f:
+#         json.dump(metadata, f, indent=2)
 
 
 def train_core(
