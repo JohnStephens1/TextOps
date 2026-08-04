f3a70f7554b54c8d3e9cabcf55a619fc534495bc
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-04 10:45:07 +0000

    chore: add logging to train

--- src/text_classifier/scripts/train.py
+++ src/text_classifier/scripts/train.py
@@ -1,13 +1,25 @@
+import logging
+
 from text_classifier.config.config import MODEL_DATASET_PATH
+from text_classifier.config.logging_config import setup_logging
 from text_classifier.data.dataset import load_dataset
 from text_classifier.model.training import train_from_config
 
+setup_logging()
+
+
+logger = logging.getLogger(__name__)
+
 
 def main() -> None:
+    logger.info("Setting up training...")
+
     df = load_dataset(MODEL_DATASET_PATH)
 
     # encoder, train_data, my_model, metrics, figs
     _, _, _, _, _ = train_from_config(df)
+
+    logger.info("Training completed")
 
 
 if __name__ == "__main__":
