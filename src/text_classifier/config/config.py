1167e2cb67dbeeb8db71289f37511cda8d1759e3
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-05 09:09:27 +0000

    chore: add data split paths to config

--- src/text_classifier/config/config.py
+++ src/text_classifier/config/config.py
@@ -10,6 +10,13 @@ PREPROCESSED_DATASET_PATH = DATASETS_DIR / "preprocessed_dataset.parquet"
 FEATURE_DATASET_PATH = DATASETS_DIR / "feature_dataset.parquet"
 MODEL_DATASET_PATH = DATASETS_DIR / "model_dataset.parquet"
 
+SPLITS_DIR = DATASETS_DIR / "splits"
+
+X_TRAIN_PATH = SPLITS_DIR / "X_train.parquet"
+X_TEST_PATH = SPLITS_DIR / "X_test.parquet"
+Y_TRAIN_PATH = SPLITS_DIR / "y_train.parquet"
+Y_TEST_PATH = SPLITS_DIR / "y_test.parquet"
+
 
 ARTIFACTS_DIR = PROJECT_ROOT / "artifacts"
 
