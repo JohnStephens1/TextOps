ccfd73519ad0b6206f60e6059402cdcc2226b647
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-08 18:00:44 +0000

    chore: add recall per class eligibility check

--- src/text_classifier/promotion/promotion.py
+++ src/text_classifier/promotion/promotion.py
@@ -1,19 +1,28 @@
+import logging
 from typing import Any
 
 from text_classifier.protocols import Predictor
+
+logger = logging.getLogger(__name__)
 
 
 def check_promotion_eligibility(
     cfg: dict[str, Any], test_metrics: dict[str, Any]
 ) -> bool:
-    for metric in test_metrics["class_recall"]:
-        if metric < cfg["class_recall"]["minimum"]:
+    for name, value in test_metrics.items():
+        if (
+            name.startswith("test_recall_class_")
+            and value < cfg["class_recall"]["minimum"]
+        ):
+            logger.info(
+                f"Promotion not eligible: {name} < {cfg['class_recall']['minimum']} : {value:.4f}"
+            )
             return False
 
     # current best required
     # TODO check for default
     # new - current > min_improvement
-    return test_metrics["macro_f1"] > cfg["macro_f1"]["min_improvement"]
+    return test_metrics["test_f1"] > cfg["macro_f1"]["min_improvement"]
 
 
 def promote(model: Predictor):
