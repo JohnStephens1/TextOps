0eb75048ec21315040909d5f225e9e74310b396f
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-12 14:45:55 +0000

    chore: extract class recall check, implement f1 improvement check

--- src/text_classifier/promotion/promotion.py
+++ src/text_classifier/promotion/promotion.py
@@ -1,12 +1,17 @@
 import logging
 from typing import Any
 
+from text_classifier.config.config import (
+    PRODUCTION_MODEL_METRICS_PATH,
+    PRODUCTION_MODEL_PATH,
+)
 from text_classifier.protocols import Predictor
+from text_classifier.save_load import load_json
 
 logger = logging.getLogger(__name__)
 
 
-def check_promotion_eligibility(
+def check_class_recall_eligibility(
     cfg: dict[str, Any], test_metrics: dict[str, Any]
 ) -> bool:
     for name, value in test_metrics.items():
@@ -15,14 +20,37 @@ def check_promotion_eligibility(
             and value < cfg["class_recall"]["minimum"]
         ):
             logger.info(
-                f"Promotion not eligible: {name} < {cfg['class_recall']['minimum']} : {value:.4f}"
+                f"Promotion not eligible. Class recall insufficient for class: {name}"
+                f"Result: {value:.4f} < {cfg['class_recall']['minimum']}"
             )
             return False
 
-    # current best required
-    # TODO check for default
-    # new - current > min_improvement
-    return test_metrics["test_f1"] > cfg["macro_f1"]["min_improvement"]
+    return True
+
+
+def check_promotion_eligibility(
+    cfg: dict[str, Any], test_metrics: dict[str, Any]
+) -> bool:
+    if not check_class_recall_eligibility(cfg, test_metrics):
+        return False
+
+    # TODO extract, check, compare
+    if PRODUCTION_MODEL_PATH.exists() and PRODUCTION_MODEL_METRICS_PATH.exists():
+        production_metrics = load_json(PRODUCTION_MODEL_METRICS_PATH)
+
+        if (
+            test_metrics["test_f1"] - production_metrics["test_f1"]
+            < cfg["macro_f1"]["min_improvement"]
+        ):
+            logger.info(
+                "Promotion not eligible. F1 improvement insufficient.\n"
+                f"Contendor: {test_metrics['test_f1']}\n"
+                f"Production: {production_metrics['test_f1']}\n"
+                f"Difference: {test_metrics['test_f1'] - production_metrics['test_f1']} < {cfg['macro_f1']['min_improvement']}"
+            )
+            return False
+
+    return True
 
 
 def promote(model: Predictor) -> None:
