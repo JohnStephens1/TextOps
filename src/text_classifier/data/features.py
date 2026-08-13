4943e6a9c692010ed6ba76df2e942e1effd9b59e
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-13 13:19:59 +0000

    chore: remove days_since_start feature and associated stateful MinMaxScaler, due to servicing issues and lack of ultimate use

--- src/text_classifier/data/features.py
+++ src/text_classifier/data/features.py
@@ -49,9 +49,9 @@ def add_time_features(df: pd.DataFrame, time_col: str = "created_on") -> pd.Data
     # either log start as artifact or remove entirely (not serviceable)
     # or learn, save, transform using sklearn pipeline
     # days since start
-    df["days_since_start"] = ((df[time_col] - df[time_col].min()).dt.days).astype(
-        "float64"
-    )
+    # df["days_since_start"] = ((df[time_col] - df[time_col].min()).dt.days).astype(
+    #     "float64"
+    # )
 
     return df
 
--- src/text_classifier/model/pipeline.py
+++ src/text_classifier/model/pipeline.py
@@ -1,27 +1,28 @@
 from sklearn.base import BaseEstimator
-from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler
+
+# from sklearn.compose import ColumnTransformer
+# from sklearn.preprocessing import MinMaxScaler
 
 
-def get_preprocessor(
-    target_cols: list[str] | None = None,
-) -> ColumnTransformer:
-    if target_cols is None:
-        target_cols = ["days_since_start"]
+# def get_preprocessor(
+#     target_cols: list[str] | None = None,
+# ) -> ColumnTransformer:
+#     if target_cols is None:
+#         target_cols = ["days_since_start"]
 
-    return ColumnTransformer(
-        [
-            ("scaler", MinMaxScaler(), target_cols),
-        ],
-        remainder="passthrough",
-    )
+#     return ColumnTransformer(
+#         [
+#             ("scaler", MinMaxScaler(), target_cols),
+#         ],
+#         remainder="passthrough",
+#     )
 
 
 def get_model_pipe(estimator: BaseEstimator) -> Pipeline:
     return Pipeline(
         [
-            ("preprocessor", get_preprocessor()),
+            # ("preprocessor", get_preprocessor()),
             ("model", estimator),
         ]
     )
