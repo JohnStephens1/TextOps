29dcace9c6a963f1fe3af0d0a0e68425bf741f6d
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-16 19:25:27 +0000

    chore: add basic gradio interface

--- app/src/app/app.py
+++ app/src/app/app.py
@@ -1,5 +1,22 @@
+import gradio as gr
+
+
+def greet(name: str, intensity: str) -> str:
+    return "Hello, " + name + "!" * int(intensity)
+
+
+def get_app() -> gr.Interface:
+    return gr.Interface(
+        fn=greet,
+        inputs=["text", "slider"],
+        outputs=["text"],
+        flagging_mode="never",
+    )
+
+
 def main() -> None:
-    print("Hello from app!")
+    app = get_app()
+    app.launch()
 
 
 if __name__ == "__main__":
