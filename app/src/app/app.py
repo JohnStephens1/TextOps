98969cd489b825448a6849dd8d3701b94479e680
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-16 22:00:42 +0000

    chore: add fastapi connection and update UI

--- app/src/app/app.py
+++ app/src/app/app.py
@@ -1,25 +1,51 @@
 import gradio as gr
+import requests
+
+FASTAPI_URL = "http://localhost:8000/predict"
 
 
-def greet(name: str, intensity: str) -> str:
-    return "Hello, " + name + "!" * int(intensity)
-
-
-def get_app() -> gr.Interface:
-    return gr.Interface(
-        fn=greet,
-        inputs=["text", "slider"],
-        outputs=["text"],
-        flagging_mode="never",
+def send_to_api(title: str, description: str) -> dict[str, str | float]:
+    response = requests.post(
+        FASTAPI_URL,
+        # json={"title": 5, "description": description},
+        json={"title": title, "description": description},
     )
+
+    if response.ok:
+        return response.json()
+    else:
+        print(f"""
+            Error in response:
+            code: {response.status_code}
+            text: {response.text}
+        """)
+
+        return {"title": "error", "description": 0.5}
+
+
+def get_demo() -> gr.Blocks:
+    with gr.Blocks() as demo:
+        user_input_title = gr.Textbox(label="Title")
+        user_input_description = gr.Textbox(label="Description")
+
+        button = gr.Button("Submit")
+        output = gr.Textbox(label="Response")
+
+        button.click(
+            fn=send_to_api,
+            inputs=[user_input_title, user_input_description],
+            outputs=output,
+        )
+
+    return demo
 
 
 # duplicate for run compatibility
 def main() -> None:
-    get_app().launch()
+    get_demo().launch()
 
 
-# app here for hot reloading functionality
+# demo here for hot reloading functionality
 if __name__ == "__main__":
-    app = get_app()
-    app.launch()
+    demo = get_demo()
+    demo.launch()
