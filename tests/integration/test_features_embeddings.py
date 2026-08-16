636cf54272462c7abb64deb5acb8cb5b47a44a63
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-16 22:53:42 +0000

    chore: update embedding test to reflect staging changes

--- tests/integration/test_features_embeddings.py
+++ tests/integration/test_features_embeddings.py
@@ -1,12 +1,18 @@
-from text_classifier.config.config import PREPROCESSED_DATASET_PATH
-from text_classifier.data.features import add_features
+from sentence_transformers import SentenceTransformer
+
+from text_classifier.config.config import (
+    EMBEDDING_MODEL_STR,
+    FEATURE_DATASET_PATH,
+)
+from text_classifier.data.embeddings import add_text_embeddings
 from text_classifier.save_load import load_parquet
 
 
 def test_identical_output_for_cached_and_new_embeddings() -> None:
-    df = load_parquet(PREPROCESSED_DATASET_PATH)
+    df = load_parquet(FEATURE_DATASET_PATH)
+    embedding_model = SentenceTransformer(EMBEDDING_MODEL_STR)
 
-    df_cached_embs = add_features(df, regenerate_embs=False)
-    df_new_embs = add_features(df, regenerate_embs=True)
+    df_cached_embs = add_text_embeddings(df, embedding_model, regenerate=False)
+    df_new_embs = add_text_embeddings(df, embedding_model, regenerate=True)
 
     assert df_cached_embs.equals(df_new_embs)
