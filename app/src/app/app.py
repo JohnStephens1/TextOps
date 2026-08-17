409b25555c91d4ea59620326eda5b19a3f5336c6
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-17 04:21:16 +0000

    chore: implement api connection and prediction output formatting

--- app/src/app/app.py
+++ app/src/app/app.py
@@ -4,45 +4,65 @@ import requests
 FASTAPI_URL = "http://localhost:8000/predict"
 
 
-def send_to_api(title: str, description: str) -> dict[str, str | float]:
-    response = requests.post(
+def get_response(title: str, description: str) -> requests.Response:
+    return requests.post(
         FASTAPI_URL,
-        # json={"title": 5, "description": description},
         json={"title": title, "description": description},
     )
 
-    if response.ok:
-        return response.json()
-    else:
-        print(f"""
-            Error in response:
-            code: {response.status_code}
-            text: {response.text}
-        """)
 
-        return {"title": "error", "description": 0.5}
+def format_response(title: str, description: str, response: requests.Response) -> str:
+    out_str = f"""
+        Input:
+        - title: {title}
+        - description: {description}\n
+    """
+
+    if response.ok:
+        result = response.json()
+
+        out_str += f"""
+            Output:
+            - prediction: {result["label"]}
+            - certainty: {f"{max(result['pred_proba']):.4f}"}
+
+            - all possible labels: {", ".join(result["all_labels"])}
+            - assigned probabilities: {", ".join([f"{x:.4f}" for x in result["pred_proba"]])}
+        """
+    else:
+        # TODO log error
+        out_str += f"""
+            Error in response:
+            - code: {response.status_code}
+            - text: {response.text}
+        """
+
+    return out_str
+
+
+def handle_prediction(title: str, description: str) -> str:
+    response = get_response(title, description)
+    out_str = format_response(title, description, response)
+
+    return out_str
 
 
 def get_demo() -> gr.Blocks:
     with gr.Blocks() as demo:
+        # TODO add placeholder text
         user_input_title = gr.Textbox(label="Title")
         user_input_description = gr.Textbox(label="Description")
 
         button = gr.Button("Submit")
-        output = gr.Textbox(label="Response")
+        out = gr.Textbox(label="Result")
 
         button.click(
-            fn=send_to_api,
+            fn=handle_prediction,
             inputs=[user_input_title, user_input_description],
-            outputs=output,
+            outputs=out,
         )
 
     return demo
-
-
-# duplicate for run compatibility
-def main() -> None:
-    get_demo().launch()
 
 
 # demo here for hot reloading functionality
