8dd837c5e0532b5bff4aa5e25c95e02e0291c17d
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-17 00:31:51 +0000

    chore: extract data_pipe

---
+++ src/text_classifier/data/data_pipe.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+
+from text_classifier.data.embeddings import add_text_embeddings
+from text_classifier.data.features import add_features
+from text_classifier.data.model_data import drop_non_feature_cols
+from text_classifier.data.preprocessing import preprocess_features
+
+
+def get_df_from_input(title: str, description: str, date_time: str) -> pd.DataFrame:
+    return pd.DataFrame(
+        {
+            "created_on": [date_time],
+            "title": [title],
+            "description": [description],
+        }
+    )
+
+
+def get_current_date_time() -> datetime:
+    return datetime.now().astimezone()
+
+
+def raw_to_model_input_pipe(
+    embedding_model: SentenceTransformer, title: str, description: str, date_time: str
+) -> pd.DataFrame:
+    df = get_df_from_input(title, description, date_time)
+    df = preprocess_features(df)
+    df = add_features(df)
+    df = add_text_embeddings(df, embedding_model, regenerate=True)
+    df = drop_non_feature_cols(df)
+
+    return df
