488d214a75e501beb86ce52479873e7dbcd3e9d8
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-18 22:20:17 +0000

    chore: implement RawModelInput into data_pipe

--- src/text_classifier/data/data_pipe.py
+++ src/text_classifier/data/data_pipe.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 
@@ -7,29 +5,24 @@ from text_classifier.data.embeddings import add_text_embeddings
 from text_classifier.data.features import add_features
 from text_classifier.data.model_data import drop_non_feature_cols
 from text_classifier.data.preprocessing import preprocess_features
+from text_classifier.schema import RawModelInput
 
 
-def get_df_from_input(
-    title: str,
-    description: str,
-    date_time: datetime,
-) -> pd.DataFrame:
+def get_df_from_input(raw_model_input: RawModelInput) -> pd.DataFrame:
     return pd.DataFrame(
         {
-            "created_on": [date_time],
-            "title": [title],
-            "description": [description],
+            "created_on": [raw_model_input.date_time],
+            "title": [raw_model_input.title],
+            "description": [raw_model_input.description],
         }
     )
 
 
 def raw_to_model_input_pipe(
     embedding_model: SentenceTransformer,
-    title: str,
-    description: str,
-    date_time: datetime,
+    raw_model_input: RawModelInput,
 ) -> pd.DataFrame:
-    df = get_df_from_input(title, description, date_time)
+    df = get_df_from_input(raw_model_input)
     df = preprocess_features(df)
     df = add_features(df)
     df = add_text_embeddings(df, embedding_model, regenerate=True)
