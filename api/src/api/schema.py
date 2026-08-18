7f6e87a7ae7b633d919d78d5bc7fe2c539ed7736
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-18 21:56:42 +0000

    chore: add RawModelInput class

--- api/src/api/schema.py
+++ api/src/api/schema.py
@@ -1,6 +1,8 @@
+import datetime
 from dataclasses import dataclass
+from typing import Self
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from sentence_transformers import SentenceTransformer
 from sklearn.preprocessing import LabelEncoder
 
@@ -25,3 +27,18 @@ class PredictionResources:
     model: Predictor
     label_encoder: LabelEncoder
     embedding_model: SentenceTransformer
+
+
+class RawModelInput(BaseModel):
+    title: str
+    description: str
+    date_time: datetime.datetime = Field(
+        default_factory=lambda: datetime.datetime.now(datetime.UTC)
+    )
+
+    @classmethod
+    def from_request(cls, request: PredictionRequest) -> Self:
+        return cls(
+            title=request.title,
+            description=request.description,
+        )
