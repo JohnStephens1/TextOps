cd4dc1ada88d0474098bd4db141809d73805b943
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-25 14:28:41 +0000

    chore: add name to getLogger

--- common/src/common/env_loader.py
+++ common/src/common/env_loader.py
@@ -8,7 +8,7 @@ from .logging_config import setup_logging
 setup_logging()
 
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
 
 class MissingEnvironmentVariableError(RuntimeError):
