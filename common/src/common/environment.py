e93f670807ee2a28c3e557d7f023a430774ae01d
john <96323078+JohnStephens1@users.noreply.github.com>
2026-08-25 06:44:50 +0000

    chore: add logging_config and environment to common

---
+++ common/src/common/environment.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def _get_env_var(env_var_str: str) -> str:
+    env_var = os.getenv(env_var_str)
+
+    if env_var is None:
+        raise RuntimeError(f"Environment variable {env_var_str} is not set.")
+
+    return env_var
+
+
+API_URL = _get_env_var("API_URL")
+MLFLOW_URL = _get_env_var("MLFLOW_URL")
